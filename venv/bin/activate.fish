b1f7d05dfaa70fc46a89cc0de5f8a1ea423b0e87
benjibyte <benjipixel@proton.me>
2026-04-23 19:32:55 -0600

    moved to python3.14.4

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/benjibyte/code/repos/chickenmatch/venv
+set -gx VIRTUAL_ENV /home/benjibyte/code/chickenmatch/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
